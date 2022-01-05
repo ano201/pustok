5f68b81c0504d76cd1491e982c3b5de62bff2ea4
ano201 <ahmu689@gmail.com>
2022-01-06 00:05:16 +0600

    banner conditionally rendered

--- components/Home/Banner.js
+++ components/Home/Banner.js
@@ -1,9 +1,31 @@
+import classNames from "classnames";
+import { useState, useEffect } from "react";
+import { useWindowSize } from "react-use";
+
 const Banner = () => {
+  const [large, setLarge] = useState(false);
+
+  const size = useWindowSize();
+
+  useEffect(() => {
+    if (size.width >= 800) {
+      setLarge(true);
+    } else {
+      setLarge(false);
+    }
+  }, [size.width]);
+
   return (
     <div className="relative">
-      <img className="w-full h-screen" src="/Images/library.jpg" alt="" />
-      <div className="absolute top-1/4 w-full">
-        <div className="p-12 bg-gray-500/60 text-gray-100 font-semibold text-xl mx-auto rounded-3xl w-fit">
+      <img className="w-full" src="/Images/library.jpg" alt="" />
+      <div className="absolute top-0 flex items-center justify-center w-full h-full px-2">
+        {/* classnames package should be install */}
+        <div
+          className={classNames("bg-gray-500/60 text-gray-100 w-fit rounded-md", {
+            "font-semibold text-lg mx-auto p-8": large,
+            "text-xs p-2": !large,
+          })}
+        >
           <div className="text-center">
             <h2>“The more that you read, the more things you will know.</h2>
             <h2>The more that you learn, the more places you&apos;ll go.”</h2>
